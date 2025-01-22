cb8b634651c2e6441733f128515fe02083b551c1
Jeremy Blanchard <blanchard.jeremy@gmail.com>
2025-01-22 15:29:37 -0800

    Redo home page

--- components/layouts/AppLayout.js
+++ components/layouts/AppLayout.js
@@ -1,11 +1,21 @@
 'use client';
 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { Navigation } from "@/components/Navigation";
 import { Footer } from "@/components/Footer";
+import { useLayoutSegments } from "next/navigation";
 
 export default function AppLayout({ children }) {
   const router = useRouter();
+  const pathname = usePathname();
+  
+  // Home page should be full width. This is super hacky.
+  const isFullWidth = pathname === '/';
+
+  const styleVariants = {
+    fullWidth: 'w-full mx-auto',
+    maxWidth: 'max-w-5xl mx-auto py-6 px-4 sm:px-6 lg:px-8',
+  }
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -13,7 +23,7 @@ export default function AppLayout({ children }) {
       
       {/* Main content */}
       <main className="flex-1 w-full bg-gray-50">
-        <div className="max-w-5xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div className={isFullWidth ? styleVariants.fullWidth : styleVariants.maxWidth}>
           {children}
         </div>
       </main>
